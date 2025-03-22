f228fc0af00defa27d1fab7f421c06a2b86ccb11
Jordan Carter <jordancarter707@gmail.com>
2025-03-22 17:26:33 -0400

    Display the pokemon generated.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,44 @@
-import { Header } from '@/components/layout/Header';
+import { Header } from '@/components/ui/Header';
+import { PokemonCard, type ItemSprite, type PokemonSprite, type Types } from '@/components/ui/PokemonCard';
+import { getRandomAbility } from '@/lib/utils/randomAbility';
+import { getRandomItem } from '@/lib/utils/randomItem';
+import { getRandomMoves } from '@/lib/utils/randomMoves';
+import { getRandomPokeApiPokemonId } from '@/lib/utils/randomPokemonId';
+import { getPokemon } from '@/services/pokemon';
 
-export default function Home() {
+const generateRandomPokemon = async () => {
+  const pokeApiPokemonId = getRandomPokeApiPokemonId();
+  const pokemonFromPokeApi = await getPokemon(pokeApiPokemonId);
+
+  const pokemonImage =
+    pokemonFromPokeApi.sprites.other.showdown.front_default ||
+    pokemonFromPokeApi.sprites.other['official-artwork'].front_default;
+
+  const itemInfo = await getRandomItem();
+
+  return {
+    name: pokemonFromPokeApi.name,
+    type: pokemonFromPokeApi.types.map((t) => t.type.name.charAt(0).toUpperCase() + t.type.name.slice(1)) as Types,
+    ability: getRandomAbility(pokemonFromPokeApi.abilities),
+    moves: await getRandomMoves(pokemonFromPokeApi.moves),
+    pokemonImage: pokemonImage as PokemonSprite,
+    itemImage: itemInfo.sprite as ItemSprite,
+    item: itemInfo.name,
+  };
+};
+
+export default async function Home() {
+  const pokemonPromises = Array.from({ length: 6 }, () => generateRandomPokemon());
+  const randomPokemon = await Promise.all(pokemonPromises);
+
   return (
-    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <main className="flex flex-col gap-y-4 row-start-2 items-center sm:items-start">
       <Header />
-      <section>Randomize and Export Buttons</section>
-      <section>Team Preview</section>
+      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full">
+        {randomPokemon.map((pokemon) => (
+          <PokemonCard key={pokemon.name} {...pokemon} />
+        ))}
+      </section>
     </main>
   );
 }
